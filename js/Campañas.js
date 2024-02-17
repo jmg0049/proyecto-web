0b3b4e71cefc61684928f439803dce1387e664cc
Jose Moreno <145755325+jmg0049@users.noreply.github.com>
2024-02-17 03:26:23 +0100

    Add files via upload

---
+++ js/Campañas.js
@@ -0,0 +1,24 @@
+var mensaje1 = function () {
+    var texto1 = document.getElementById("Campaña1");
+    texto1.value = " ";
+    var username = 'Nobody';
+    console.log(username);
+};
+var mensaje2 = function () {
+    var texto2 = document.getElementById("Campaña2");
+    texto2.value = " ";
+    var username = 'Nobody';
+    console.log(username);
+};
+var mensaje3 = function () {
+    var texto3 = document.getElementById("Campaña3");
+    texto3.value = " ";
+    var username = 'Nobody';
+    console.log(username);
+};
+var mensaje4 = function () {
+    var texto4 = document.getElementById("Campaña4");
+    texto4.value = " ";
+    var username = 'Nobody';
+    console.log(username);
+};
